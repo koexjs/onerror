684aa5e93aa7387752102d18d836ce3d76e01b25
tobewhatwewant <tobewhatwewant@gmail.com>
2018-12-09 16:35:53 +0800

    :bookmark:(version): bumped version v0.0.2, support status/message/code/errors and support json or template function

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,54 @@
 import { Context } from 'koa';
 
+export interface InvalidateError {
+  code: string;
+  field: string;
+  message: string;
+}
+
 export interface Options {
-  log: (method: string, path: string, error: Error | string) => void;
+  type?: 'json' | 'template';
+  template?: (status: number, message: string, code: string, errors?: InvalidateError[]) => string;
+  log?: (method: string, path: string, error: Error | string) => void;
 }
 
 const createOnError = (options?: Options) => {
-  const getLogger = (ctx: Context) => options && options.log || ((...args: any[]) => ctx.logger ? ctx.logger.error(...args) : console.error(...args));
+  const type = options && options.type || 'json';
+  const template = options && options.template;
+  const log = options && options.log;
+
+  const getLogger = (ctx: Context) =>
+    ((...args: any[]) =>
+      log
+        ? (log as any)(...args)
+        : ctx.logger
+          ? ctx.logger.error(...args)
+          : console.error(...args));
 
   return async function onerror(ctx: Context, next: () => Promise<void>) {
     try {
       await next();
     } catch (error) {
+      // log
       const logger = getLogger(ctx);
       logger(ctx.method, ctx.path, error instanceof Error ? error.stack : error);
+    
+      const status = error.status || 500;
+      const message = error.status ? error.message : 'Internal Server Error';
+      const code = error.code;
+      const errors = error.errors;
 
-      ctx.status = error.status || 500;
-      ctx.body = {
-        message: error.status ? error.message : 'Internal Server Error',
-      };
+      ctx.status = status;
+
+      if (type === 'template') {
+        ctx.body = template ? template(status, message, code, errors) : error.message;
+      } else {
+        ctx.body = {
+          code,
+          message,
+          errors,
+        };
+      }
     }
   };
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,7 @@
 import * as Koa from 'koa';
 import * as request from 'supertest';
+import * as bodyParser from 'koa-body';
+import validate from '@zcorky/koa-validate';
 import { get, post } from '@zcorky/koa-router';
 import 'should';
 
@@ -24,6 +26,7 @@ describe('koa onerror', () => {
     it('should throw default', async () => {
       await request(app.listen())
         .get('/')
+        .set('accept', '*/*')
         .expect(500, { message: 'Internal Server Error' });
     });
 
@@ -53,6 +56,48 @@ describe('koa onerror', () => {
       await request(app.listen())
         .get('/')
         .expect(500, { message: 'Internal Server Error' });
+    });
+  });
+
+  describe('with koa-validate', () => {
+    const app = new Koa();
+
+    app.use(onerror({
+      log(method, path, error) {
+        method.should.equal('POST');
+        path.should.equal('/');
+        // error.should.equal('error');
+      },
+    }));
+
+    app.use(bodyParser())
+    app.use(validate());
+
+    app.use(post('/',
+      async (ctx, next) => {
+        console.log('post');
+        ctx.validate({
+          name: 'string',
+        });
+
+        await next();
+      }, async (ctx) => {
+        ctx.body = '123';
+      }));
+
+    it('should throw', async () => {
+      await request(app.listen())
+        .post('/')
+        .send({ name: 123 })
+        .expect(422, {
+          code: 'invalid_param',
+          message: 'Validation Failed',
+          errors: [{
+            code: 'invalid',
+            field: 'name',
+            message: 'should be a string',
+          }],
+        });
     });
   });
 });
